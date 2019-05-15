fd9ad84f3ca499dafd46d3d713851683668ad03b
Keith Walker <Keithrwalker418@gmail.com>
2019-05-14 19:25:04 -0500

    step3

--- src/javascripts/components/boards/boards.js
+++ src/javascripts/components/boards/boards.js
@@ -1,9 +1,27 @@
 import boardData from '../../helpers/data/boardsData';
 
+import util from '../../helpers/util';
+
+let boardAr = [];
+
+const boardBuild = (ar) => {
+  let boardSt = '';
+  ar.forEach((board) => {
+    boardSt += '<div class="col-3">';
+    boardSt += `<div class="card board-card" id="${board.id}">`;
+    boardSt += `<h4>${board.name}</h4>`;
+    boardSt += '</div>';
+    boardSt += '</div>';
+  });
+  util.printToDom('user-boards', boardSt);
+};
+
 const initBoards = () => {
   boardData.loadBoards()
     .then((resp) => {
-      console.error('resp', resp.data.boards);
+      const boardDataAr = resp.data.boards;
+      boardAr = boardDataAr;
+      boardBuild(boardAr);
     })
     .catch(err => console.error('error from loadBoards', err));
 };
