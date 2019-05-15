93612cf92817b52c5c7db3471a96b65a87c04d66
Keith Walker <Keithrwalker418@gmail.com>
2019-05-14 19:46:23 -0500

    pin btns hiding cards

--- src/javascripts/components/boards/boards.js
+++ src/javascripts/components/boards/boards.js
@@ -4,16 +4,32 @@ import util from '../../helpers/util';
 
 let boardAr = [];
 
+const seePinDiv = (e) => {
+  const boardId = e.target.closest('.card').id;
+  console.error('you clicked the button', boardId);
+  document.getElementById('boards-page').classList.add('hide');
+  document.getElementById('pins-page').classList.add('hide');
+};
+
+const bindEvents = () => {
+  const allButtons = document.getElementsByClassName('see-pins');
+  for (let i = 0; i < allButtons.length; i += 1) {
+    allButtons[i].addEventListener('click', seePinDiv);
+  }
+};
+
 const boardBuild = (ar) => {
   let boardSt = '';
   ar.forEach((board) => {
-    boardSt += '<div class="col-3">';
+    boardSt += `<div class="col-3" id="${board.id}">`;
     boardSt += `<div class="card board-card" id="${board.id}">`;
     boardSt += `<h4>${board.name}</h4>`;
+    boardSt += '<button class="btn btn-danger see-pins">Pins</button>';
     boardSt += '</div>';
     boardSt += '</div>';
   });
   util.printToDom('user-boards', boardSt);
+  bindEvents();
 };
 
 const initBoards = () => {
